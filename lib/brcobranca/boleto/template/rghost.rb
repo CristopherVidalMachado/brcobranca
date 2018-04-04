8573c335ed74c9a2bb58e1e6e5cfc469d75c5a51
Cristopher Vidal Machado <cristophervidalmachadoKIONUX@bitbucket.com>
2018-04-04 15:32:39 -0300

    Alterando o projeto

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -142,7 +142,7 @@ module Brcobranca
           @x = 4.75
           @y = 20.77
 
-          doc.moveto x: "4.75 cm", y: "25.00 cm"
+          doc.moveto x: "4.50 cm", y: "25.20 cm"
           doc.show boleto.codigo_barras.linha_digitavel
           move_more(doc, -4.25, 0.10)
           doc.image boleto.logotipo, x: "#{@x} cm", y: "#{@y} cm"
