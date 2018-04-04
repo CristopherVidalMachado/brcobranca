0bca934a62c7de6876d8d8ddaed341530040c920
Cristopher Vidal Machado <cristophervidalmachadoKIONUX@bitbucket.com>
2018-04-04 17:42:20 -0300

    testar

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -220,8 +220,7 @@ module Brcobranca
 
 
 
-          move_more(doc, 0, -0.8)
-          doc.show "#{boleto.sacado_endereco}"
+          # move_more(doc, 0, -0.8)
           if boleto.demonstrativo
             doc.text_area boleto.demonstrativo, width: '18.5 cm', text_align: :left, x: "#{@x - 0.8} cm", y: "#{@y - 0.9} cm", row_height: '0.4 cm'
           end
