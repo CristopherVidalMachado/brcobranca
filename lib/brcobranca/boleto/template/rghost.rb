cc994861eafbfe5ba5c89c13ccc78c30efb68113
Cristopher Vidal Machado <cristophervidalmachadoKIONUX@bitbucket.com>
2018-04-04 15:30:13 -0300

    Alterando o projeto

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -142,7 +142,7 @@ module Brcobranca
           @x = 4.75
           @y = 20.77
 
-          doc.moveto x: "4.75 cm", y: "25.77 cm"
+          doc.moveto x: "4.75 cm", y: "23.77 cm"
           doc.show "Dados por pacotes "
           move_more(doc, -4.25, 0.10)
           doc.image boleto.logotipo, x: "#{@x} cm", y: "#{@y} cm"
